17eba250af45fcddb3e0cda4215ffe01096c3db2
Allison Olson <aolson393@gmail.com>
2013-05-19 09:36:41 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tspoonmeals::Application.config.secret_token = '95ea6bcbc637c7f99df96a3043c4638025efb6f649af51f182e5ea52b573524674e66e28482863db8b0594d36315db45839c24969684c11d58d1d37cef82e376'
